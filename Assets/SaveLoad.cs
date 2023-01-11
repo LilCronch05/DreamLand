73919e032df2e3711297f977b2e567ec3f7b1722
Alexander <abibault@gmail.com>
2023-01-11 16:32:13 -0700

    Added Loading function

--- Assets/SaveLoad.cs
+++ Assets/SaveLoad.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 using System.IO;
 using System.Xml;
 using System.Xml.Serialization;
-using UnityEditor.UI;
+using UnityEngine.UI;
+using TMPro;
 
 public class SaveLoad : MonoBehaviour
 {
     [SerializeField]
     string name;
-    string strength;
-    string dexterity;
-    string defence;
-    string health;
-    string speed;
+    //string strength;
+    //string dexterity;
+    //string defence;
+    //string health;
+    //string speed;
+
+    [SerializeField]
+    GameObject nameField;
+
+    [SerializeField]
+    TextMeshProUGUI myText;
 
 
     string filePath = "SaveData";
@@ -22,10 +29,19 @@ public class SaveLoad : MonoBehaviour
 
     SaveContainer myContainer = new SaveContainer();
 
+    private void Start()
+    {
+        myText = nameField.GetComponentInChildren<TextMeshProUGUI>();
+        LoadInfo();
+        myText.text = myContainer.name;
+    }
+
     public void ChangeName(string newName)
     {
         name = newName;
+        SaveInfo();
     }
+    /*
     public void ChangeStrength(string newStrength)
     {
         strength = newStrength;
@@ -46,15 +62,16 @@ public class SaveLoad : MonoBehaviour
     {
         speed = newSpeed;
     }
+    */
 
     public void SaveInfo()
     {
         myContainer.name = name;
-        myContainer.strength = strength;
-        myContainer.dexterity = dexterity;
-        myContainer.defence = defence;
-        myContainer.health = health;
-        myContainer.speed = speed;
+        //myContainer.strength = strength;
+        //myContainer.dexterity = dexterity;
+        //myContainer.defence = defence;
+        //myContainer.health = health;
+        //myContainer.speed = speed;
 
         if(!Directory.Exists(filePath))
         {
@@ -66,15 +83,26 @@ public class SaveLoad : MonoBehaviour
         serializer.Serialize(stream, myContainer);
         stream.Close();
     }
+
+    public void LoadInfo()
+    {
+        if(Directory.Exists(filePath))
+        {
+            Stream stream = File.Open(filePath + "/" + fileName, FileMode.Open);
+            XmlSerializer serializer = new XmlSerializer(typeof(SaveContainer));
+            myContainer = (SaveContainer)serializer.Deserialize(stream);
+            stream.Close();
+        }
+    }
 }
 
 [System.Serializable]
 public class SaveContainer
 {
     public string name;
-    public string strength; 
-    public string dexterity;
-    public string defence;
-    public string health;
-    public string speed;
+    //public string strength; 
+    //public string dexterity;
+    //public string defence;
+    //public string health;
+    //public string speed;
 }
