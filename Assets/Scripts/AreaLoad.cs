aa9ad66da6a4269ad2ec542adc1a99a904e0efb8
Alexander <abibault@gmail.com>
2023-02-14 16:25:43 -0700

    fixing character controller

---
+++ Assets/Scripts/AreaLoad.cs
@@ -0,0 +1,40 @@
+using RPGCharacterAnims;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AreaLoad : MonoBehaviour
+{
+    [SerializeField]
+    Transform entrance;
+    [SerializeField]
+    Transform exit;
+    [SerializeField]
+    string areaName;
+    bool transition = false;
+    RPGCharacterInputController player;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (transition /*&& !player.Fading()*/)
+        {
+            SceneManager.LoadScene(areaName, LoadSceneMode.Additive);
+            SceneManager.UnloadSceneAsync("Field");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerInfo.piInstance.spawnLocation = exit.position;
+            PlayerInfo.piInstance.currentScene = areaName;
+
+            player = other.GetComponent<RPGCharacterInputController>();
+            //player.Fade(true);
+            transition = true;
+        }
+    }
+}
